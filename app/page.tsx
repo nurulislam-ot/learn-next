cf83944cb0c2b65b688370f6f2dcd9ae195306cb
nurulislam-ot <nurul@orangetoolz.com>
2025-05-05 21:39:47 +0600

    chore: add pnpm workspace configuration to ignore built dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,7 @@ export default function Home() {
   return (
     <main className='p-4'>
       <h1 className='font-lora text-5xl'>Hello World</h1>
-      <button
-        onClick={() => {
-          alert('Hello Bangladesh. How are you!')
-        }}
-      >
-        Hello
-      </button>
+      <button>Hello</button>
     </main>
   )
 }

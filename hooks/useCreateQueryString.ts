f43c1cf1e1dbac900c709d9111f5f0ff10e5d9eb
nurulislam-ot <nurul@orangetoolz.com>
2025-05-05 21:49:05 +0600

    fix: correct URLSearchParams initialization in useCreateQueryString hook

--- hooks/useCreateQueryString.ts
+++ hooks/useCreateQueryString.ts
@@ -1,12 +1,12 @@
-import { useCallback } from 'react'
-import { useSearchParams, usePathname } from 'next/navigation'
+import { useCallback } from "react"
+import { useSearchParams, usePathname } from "next/navigation"
 
 const useCreateQueryString = (withUrl: boolean = false) => {
   const path = usePathname()
   const searchParams = useSearchParams()
   const createQueryString = useCallback(
     (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams)
+      const params = new URLSearchParams(`${searchParams}`)
       params.set(name, value)
 
       if (withUrl) return `${path}?${params.toString()}`
